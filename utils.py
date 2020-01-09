a8550c2db476a204c4706213ad099755edbc094a
Patrick Liu <patrickl@xsense.ai>
2020-01-08 22:21:11 -0800

    Move iou utils

--- utils.py
+++ utils.py
@@ -9,6 +9,51 @@ from math import sin, cos
 from PIL import ImageDraw, Image
 
 
+def get_iou(bb1, bb2):
+    # determine the coordinates of the intersection rectangle
+    x_left = max(bb1['x1'], bb2['x1'])
+    y_top = max(bb1['y1'], bb2['y1'])
+    x_right = min(bb1['x2'], bb2['x2'])
+    y_bottom = min(bb1['y2'], bb2['y2'])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    bb1_area = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
+    bb2_area = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    assert iou >= 0.0
+    assert iou <= 1.0
+    return iou
+
+def get_bb_dict(xmin, ymin, xmax, ymax):
+    bb_dict = {
+        'x1': xmin,
+        'x2': xmax,
+        'y1': ymin,
+        'y2': ymax
+    }
+    return bb_dict
+
+def read_json(json_file):
+    with open(json_file, 'r') as f_in:
+        data = json.load(f_in)
+    return data
+
+def write_json(data, json_file):
+    os.makedirs(os.path.dirname(json_file), exist_ok=True)
+    with open(json_file, 'w') as f_out:
+        json.dump(data, f_out, sort_keys=True, indent=4)
+
 def load_3dlabel(car_model, model_dir=None):
     model_dir = '../data/car_models_json' if model_dir is None else model_dir
     try:
